3bb1deee155f91fecfe195ff62fe91de22fa5d54
panjiajie <251907882@qq.com>
2023-06-01 19:19:36 +0800

    逆转换

--- src/binary.js
+++ src/binary.js
@@ -1,16 +1,21 @@
+const isSignificant = (num, base) => {
+  // num不是有效数字
+  if (typeof num !== "number" || Number.isNaN(Number(num))) {
+    return false;
+  } else if (base < 1 || !Number.isInteger(base)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
 /**
  * N进制转换为10进制
  * @param {Number} num 数字
  * @param {Number} base n进制
  */
 const transform = (num = 0, base = 10) => {
-  // num不是有效数字
-  if (typeof num !== "number" || Number.isNaN(Number(num))) {
-    return false;
-  }
-
-  // base不是正整数
-  if (base < 1 || !Number.isInteger(base)) {
+  if (!isSignificant(num, base)) {
     return false;
   }
 
@@ -28,7 +33,7 @@ const transform = (num = 0, base = 10) => {
     };
     arr.push(obj);
   }
-  
+
   for (let i = 0; i < arr.length; i++) {
     const item = arr[i];
     const { isDecimalPoint, isIntPart, number } = item;
@@ -37,15 +42,37 @@ const transform = (num = 0, base = 10) => {
     } else if (!isDecimalPoint && isIntPart) {
       // 整数部分
       sum += number * Math.pow(base, intPart.length - (i + 1));
-      debugger
     } else {
       // 浮点数部分
       sum += number * Math.pow(base, arr.length - intPart.length - 1 - i);
-      debugger
     }
   }
-  
+
+  return sum;
+};
+/**
+ * 10进制转n进制
+ * @param {Number} num
+ * @param {Number} base
+ */
+const transformReverse = (num = 0, base = 2) => {
+  if (!isSignificant(num, base)) {
+    return false;
+  }
+
+  const arr = [];
+  let start = num;
+  while (start >= base) {
+    const remainder = start % base; // 取余
+    const quotient = parseInt(start / base);
+    arr.push(remainder);
+    start = quotient;
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += Math.pow(10, arr.length - i - 1) * arr[i];
+  }
   return sum;
 };
 
-export { transform };
+export { transform, transformReverse };
